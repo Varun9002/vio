04110bb719fda88e6edc47d75f561c4d723e1591
Varun <aggarwalvarun58@gmail.com>
2023-03-19 21:58:51 +0530

    Add Search page template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './sideNav/nav/nav.component';
+import { SearchComponent } from './main/search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
